de75269b9182d42a0b4cf7b0b9994f5f1f07fb96
Isaac Evans <ine@mit.edu>
2013-05-01 16:52:36 -0400

    add capture-bindings and apply-bindings

--- lib/forms.scm
+++ lib/forms.scm
@@ -4,11 +4,19 @@
 (define (get-property form property)
   (eq-get form property))
 
+;; TODO - decide whether to convert this to a continuation form rather than calling update-form immediately
 (define (set-property form property value)
   (let ((val (eq-put! form property value)))
     (update-form form)
     val))
 
+(define (capture-bindings form)
+  (eq-plist-simple form)
+)
+
+(define (apply-bindings form bindings)
+  (for-each (lambda (binding) (set-property form (car binding) (cdr binding))) bindings)
+)
 
 (define (same-type? form-a form-b)
   (equal? (eq-ordered-plist form-a) (eq-ordered-plist form-b))
@@ -77,10 +85,13 @@
 ;;                   (make-vertex 0 0 0)))
 ;; 
 ;; (declare-form 'lhand '3D-hand-form)
-;; 
+ 
 ;; (pp-form 'thing1)
 ;; (pp-form 'lhand)
-;; 
+;; (pp (capture-bindings 'thing1))
+;; (apply-bindings 'thing1 (capture-bindings 'thing1))
+;; (pp (capture-bindings 'thing1)) ;; should be the same
+
 ;; (make-3d-form 'thing1)
 ;; (make-3d-form 'thing2)
 ;; (make-3d-hand-form 'lhand)
