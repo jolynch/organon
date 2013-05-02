1263b12e8efa37d2aae45743c7a9440265603697
Isaac Evans <ine@mit.edu>
2013-05-01 23:13:40 -0400

    added hint functions for constraint demos

--- demos/ladder.scm
+++ demos/ladder.scm
@@ -1,4 +1,5 @@
-(define *debug* #t)
+(define *demo-debug* #t)
+(define *debug* #f)
 
 (declare-form 'left-hand '3D-hand-form)
 (declare-form 'right-hand '3D-hand-form)
@@ -7,12 +8,18 @@
 (define hands-far-away (make-basic-constraint
                          '(left-hand right-hand)
                          (lambda (h1 h2)
-                           0.5)))
+                           0.5)
+                         (lambda (h1 h2)
+                           (list (cons h1 (list (list 'vertices (make-vertex 1 2 3))))
+                                 (cons h2 (list (list 'vertices (make-vertex 4 5 6))))))))
 
 (define hands-end-of-rung (make-basic-constraint
                             '(left-hand right-hand)
                             (lambda (h1 h2)
-                              0.5)))
+                              0.5)
+                            (lambda (h1 h2)
+                           (list (cons h1 (list (list 'vertices (make-vertex 1 2 3))))
+                                 (cons h2 (list (list 'vertices (make-vertex 4 5 6))))))))
 
 (define hands-on-ladder (make-compound-constraint
                           (list hands-far-away hands-end-of-rung)
@@ -23,3 +30,4 @@
 
 (display "Final hands-on-ladder value:")(write (hands-on-ladder))(newline)
 
+(iterative-solver '(left-hand right-hand) '(hands-far-away hands-end-of-rung))
