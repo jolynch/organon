44ab62abc910dff25b178c7e3dc7c5228df40b66
Joseph Lynch <jolynch@mit.edu>
2013-05-03 23:41:37 -0400

    More demo making, working on getting it to actually work

--- demos/ladder.scm
+++ demos/ladder.scm
@@ -1,17 +1,14 @@
 (define *demo-debug* #t)
-(define *debug* #f)
+(define *debug* #t)
 
 (declare-form 'left-hand '3D-hand-form)
 (declare-form 'right-hand '3D-hand-form)
 (declare-form 'rung '3D-form)
 (declare-form 'desired-distance 'basic)
+(declare-form 'desired-closeness 'basic)
 (declare-form 'axis 'basic)
 
 (define (distance v1 v2)
-  (display v1)
-  (newline)
-  (display v2)
-  (newline)
   (if (and (eq? (length v1) 3) (eq? (length v2) 3))
   (sqrt (+ (square (- (first v1) (first v2)))
            (square (- (second v1) (second v2)))
@@ -44,6 +41,7 @@
 
 (set-property 'desired-distance 'value 5)
 (set-property 'axis 'value (make-vector 1 0 0))
+(set-property 'desired-closeness 'value .5)
 
 (define hands-far-away (make-basic-constraint
                          '(left-hand right-hand desired-distance)
@@ -54,30 +52,45 @@
                                   (let* ((left-origin (car (get-property lh 'frame)))
                                          (right-origin (car (get-property rh 'frame)))
                                          (dis (distance left-origin right-origin)))
-                                    (min 1.0 (/ dis (get-property d 'value)))))
+                                    (min 1.0 (/ dis (get-value d)))))
                                  (else 0.0)))
                          (lambda (h1 h2 d)
                            (list (list h1 (list (list 'vertices (make-vertex 2 4 8))))
                                  (list h2 (list (list 'vertices (make-vertex 8 4 2))))))))
 
 (define hands-end-of-rung (make-basic-constraint
-                            '(left-hand right-hand)
-                            (lambda (h1 h2)
-                              0.5)
-                            (lambda (h1 h2)
-                           (list (list h1 (list (list 'vertices (make-vertex 1 3 5))))
-                                 (list h2 (list (list 'vertices (make-vertex 5 3 1))))))))
+                            '(left-hand right-hand desired-closeness rung)
+                            (lambda (lh rh d r)
+                              (cond
+                                ((and (is-type? lh '3D-hand-form)
+                                      (is-type? rh '3D-hand-form)
+                                      (is-type? d 'basic))
+                                 (let* ((left-origin (car (get-property lh 'frame)))
+                                        (right-origin (car (get-property rh 'frame)))
+                                        (left-rung (first (get-property r 'vertices)))
+                                        (right-rung (second (get-property r 'vertices)))
+                                        (disl (distance left-origin left-rung))
+                                        (disr (distance right-origin right-rung)))
+                                   (min
+                                     1.0
+                                     (+ (/ (min 1.0 (/ (get-value d) disl)) 2.0)
+                                        (/ (min 1.0 (/ (get-value d) disr)) 2.0)))))
+                                (else 0.0)))
+                            (lambda (h1 h2 d)
+                              (list (list h1 (list (list 'vertices (make-vertex 1 3 5))))
+                                    (list h2 (list (list 'vertices (make-vertex 5 3 1))))))))
 
 (define hands-on-ladder (make-compound-constraint
                           (list hands-far-away hands-end-of-rung)
                           (lambda (hfa heor)
                             (let ((h1 (hfa))
                                   (h2 (heor)))
-                              (and (> h1 .25) (> h2 .25))))))
+                              (/ (+ h1 h2) 2.0)))))
 
-(display "Final hands-on-ladder value:")(write (hands-on-ladder))(newline)
+;;(display "Final hands-on-ladder value:")(write (hands-on-ladder))(newline)
 
-(basic-iterative-solver '(left-hand right-hand) (list hands-on-ladder))
+
+;;(basic-iterative-solver '(left-hand right-hand) (list hands-on-ladder))
 
 ;;(iterative-solver '(left-hand right-hand) '(hands-on-ladder))
 
